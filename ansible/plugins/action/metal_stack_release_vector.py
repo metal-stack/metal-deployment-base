ea0effb9489ae7a47209be7abccaa2591824beda
Gerrit <tirreg91@googlemail.com>
2025-05-28 15:21:40 +0200

    Allow setting registry password and recurse into subvectors.

--- ansible/plugins/action/metal_stack_release_vector.py
+++ ansible/plugins/action/metal_stack_release_vector.py
@@ -16,7 +16,7 @@ from ansible import constants as C
 
 HAS_OPENCONTAINERS = True
 try:
-    from opencontainers.distribution.reggie import NewClient, WithReference, WithDigest, WithDefaultName # type: ignore[import]
+    from opencontainers.distribution.reggie import NewClient, WithReference, WithDigest, WithDefaultName, WithUsernamePassword # type: ignore[import]
     import opencontainers.image.v1 as opencontainersv1 # type: ignore[import]
 except ImportError as ex:
     HAS_OPENCONTAINERS = False
@@ -51,6 +51,8 @@ class ActionModule(ActionBase):
 
         full_ref                       = module_args.get("ref", task_vars.get("metal_stack_release_vector"))
         self._release_vector_file_name = module_args.get("file_name", "release.yaml")
+        self._registry_username        = module_args.get("username", task_vars.get("metal_stack_release_vector_registry_username"))
+        self._registry_password        = module_args.get("password", task_vars.get("metal_stack_release_vector_registry_password"))
         self.registry_scheme           = module_args.get("registry_scheme", "https")
         install_roles                  = module_args.get("install_ansible_roles", True)
 
@@ -59,40 +61,61 @@ class ActionModule(ActionBase):
             result["msg"] = "either release vector parameter must be given or metal_stack_release_vector must be defined in the variables"
             return result
 
-        self._registry, self._registry_namespace, self._version = _parse_oci_ref(full_ref, scheme=self.registry_scheme)
+        registry, registry_namespace, version = _parse_oci_ref(full_ref, scheme=self.registry_scheme)
 
-        if not self._version:
+        if not version:
             result["failed"] = True
             result["msg"] = "release vector reference must contain a specific version"
             return result
 
         try:
-            release_vector = self._download_release_vector()
+            release_vectors = self._download_release_vectors(registry, registry_namespace, version)
 
             if install_roles:
-                self._install_ansible_roles(release_vector.get("ansible-roles", []))
+                for vector in release_vectors:
+                    self._install_ansible_roles(vector.get("ansible-roles", []))
 
         except Exception as e:
             result["failed"] = True
             result["msg"] = str(to_native(e))
             return result
 
-        result["content"] = release_vector
+        if release_vectors:
+            result["content"] = release_vectors[0]
 
         return result
 
 
-    def _download_release_vector(self):
-        blob = _download_blob(
-            self._registry,
-            self._registry_namespace,
-            self._version,
+    def _download_release_vectors(self, registry, namespace, version, vectors=[]):
+        display.display("- Resolving release vector %s:%s" % (namespace, version), color=C.COLOR_CHANGED)
+
+        blob = self._download_blob(
+            registry,
+            namespace,
+            version,
             RELEASE_VECTOR_MEDIA_TYPE,
         )
 
-        # TODO: recurse
+        release_vector = safe_load(_extract_tar_gzip_file(blob.content, self._release_vector_file_name))
 
-        return _extract_tar_gzip_file(blob.content, self._release_vector_file_name)
+        vectors.append(release_vector)
+
+        for vector_name, subvector in release_vector.get("vectors", {}).items():
+            ref = subvector.get("oci")
+
+            # FIXME: just for demonstration purposes
+            if vector_name == "metal-stack":
+                ref = "ghcr.io/metal-stack/releases:develop"
+
+            if not ref:
+                display.display("- Nested release vector %s has no oci ref, skipping" % (vector_name), color=C.COLOR_SKIP)
+                continue
+
+            registry, registry_namespace, version = _parse_oci_ref(ref, scheme=self.registry_scheme)
+
+            vectors = self._download_release_vectors(registry, registry_namespace, version, vectors)
+
+        return vectors
 
 
     def _install_ansible_roles(self, roles):
@@ -107,6 +130,8 @@ class ActionModule(ActionBase):
             # FIXME: just for demonstration purposes
             if role_name == "metal-roles":
                 role_ref = "ghcr.io/metal-stack/metal-roles:oci-artifacts"
+            if role_name == "metal-extensions-roles":
+                role_ref = "r.metal-stack.io/extensions/metal-extensions-roles:oci-artifacts"
 
             if not role_version:
                 raise Exception("no version specified for role " + role_name)
@@ -124,7 +149,7 @@ class ActionModule(ActionBase):
             if role_ref:
                 registry, registry_namespace, version = _parse_oci_ref(role_ref, scheme=self.registry_scheme)
 
-                blob = _download_blob(
+                blob = self._download_blob(
                     registry,
                     registry_namespace,
                     version,
@@ -143,63 +168,66 @@ class ActionModule(ActionBase):
                     raise Exception("error cloning git repository: %s" % to_native(e))
 
 
+    def _download_blob(self, address, default_name, reference, layer_media_type):
+        opts = [WithDefaultName(default_name)]
+        if self._registry_username and self._registry_password:
+            opts.append(WithUsernamePassword(username=self._registry_username, password=self._registry_password))
+
+        client = NewClient(address,
+            *opts
+        )
+
+        req = client.NewRequest(
+            "GET",
+            "/v2/<name>/manifests/<reference>",
+            WithReference(reference),
+        ).SetHeader("Accept", opencontainersv1.MediaTypeImageManifest)
+
+        try:
+            response = client.Do(req)
+            response.raise_for_status()
+        except Exception as e:
+            raise Exception("the download of the release vector raised an error: %s" % to_native(e))
+
+        manifest = response.json()
+
+        for layer in manifest["layers"]:
+            if layer["mediaType"] == layer_media_type:
+                target = layer
+                break
+
+        if not target:
+            raise Exception("no layer with media type %s found in oci release vector" % layer_media_type)
+
+        req = client.NewRequest(
+            "GET",
+            "/v2/<name>/blobs/<digest>",
+            WithDigest(target['digest']),
+        )
+
+        req.stream = True
+
+        try:
+            blob = client.Do(req)
+            blob.raise_for_status()
+        except Exception as e:
+            raise Exception("the download of the release vector layer raised an error: %s" % to_native(e))
+
+        return blob
+
+
 def _parse_oci_ref(full_ref, scheme='https'):
     ref, *tag = full_ref.split(":", 1)
     tag = tag[0] if tag else None
     url = urlparse("%s://%s" % (scheme, ref))
     return "%s://%s" % (scheme, url.netloc), url.path.removeprefix('/'), tag
 
-def _download_blob(address, default_name, reference, layer_media_type):
-    client = NewClient(address,
-        WithDefaultName(default_name),
-    )
-
-    req = client.NewRequest(
-        "GET",
-        "/v2/<name>/manifests/<reference>",
-        WithReference(reference),
-    ).SetHeader("Accept", opencontainersv1.MediaTypeImageManifest)
-
-    # TODO: also allow WithUserAuth("myuser", "mypass") and iterate through credentials in case of unauthorized
-
-    try:
-        response = client.Do(req)
-        response.raise_for_status()
-    except Exception as e:
-        raise Exception("the download of the release vector raised an error: %s" % to_native(e))
-
-    manifest = response.json()
-
-    for layer in manifest["layers"]:
-        if layer["mediaType"] == layer_media_type:
-            target = layer
-            break
-
-    if not target:
-        raise Exception("no layer with media type %s found in oci release vector" % layer_media_type)
-
-    req = client.NewRequest(
-        "GET",
-        "/v2/<name>/blobs/<digest>",
-        WithDigest(target['digest']),
-    )
-
-    req.stream = True
-
-    try:
-        blob = client.Do(req)
-        blob.raise_for_status()
-    except Exception as e:
-        raise Exception("the download of the release vector layer raised an error: %s" % to_native(e))
-
-    return blob
-
 
 def _extract_tar_gzip_file(bytes, member):
     with tarfile.open(fileobj=BytesIO(bytes), mode='r:gz') as tar:
         with tar.extractfile(tar.getmember(member)) as f:
             try:
-                return safe_load(f.read().decode('utf-8'))
+                return f.read().decode('utf-8')
             except Exception as e:
                 raise Exception("error extracting tar member from oci layer: %s" % to_native(e))
 
